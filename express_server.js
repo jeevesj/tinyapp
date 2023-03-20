0fb8d6b33ecd42f014001405b0067060863b33ae
Christian Gravel <cjgravel@gmail.com>
2023-03-20 16:14:35 -0700

    set EJS as engine

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+pp.set("view engine", "ejs")
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
