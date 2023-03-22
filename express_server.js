36fd0689bec49e4d64da0826049e9c6517bdf89e
Christian Gravel <cjgravel@gmail.com>
2023-03-21 19:51:33 -0700

    added delete buttons and POST route to remove from object

--- express_server.js
+++ express_server.js
@@ -58,6 +58,12 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  const shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.send("Poof");
+});
+
 function generateRandomString() {
   let result = '';
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
