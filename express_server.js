fee3c3eb97a0628a2ac4fd43204998328f03d156
Christian Gravel <cjgravel@gmail.com>
2023-03-22 21:46:49 -0700

    finally, it works

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
@@ -27,7 +30,9 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-    const templateVars = {urls: urlDatabase};
+  const username = req.cookies["username"];
+  const templateVars = {urls: urlDatabase, username: username};
+    
     res.render("urls_index", templateVars);
 });
 
@@ -50,9 +55,23 @@ app.get("/urls/:id/", (req, res) => {
     res.render("urls_show", templateVars);
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+
+  res.cookie("username", username);
+
+  res.redirect("/urls"); 
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
+
 app.post("/register", (req, res) => {
   
-  console.log("debug register body",req.body);
+  console.log(req.body);
 
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
